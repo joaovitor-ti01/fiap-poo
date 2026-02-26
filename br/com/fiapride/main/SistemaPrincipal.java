19fffcde23acec8072342c567a2420300fc11309
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-26 09:51:10 -0300

    feat: adiciona métodos de comportamento

--- br/com/fiapride/main/SistemaPrincipal.java
+++ br/com/fiapride/main/SistemaPrincipal.java
@@ -5,17 +5,25 @@ public class SistemaPrincipal {
 
     public static void main(String[] args) {     
     	
-    	Portao portao1 = new Portao();
-    	Portao portao2 = new Portao();
-    	
-    	portao1.cor = "preto";
-    	portao1.materialEstrutura = "Alumínio";
-    	portao1.temVidro = false;
-    	
-    	portao2.cor = "cinza";
-    	portao2.materialEstrutura = "Aço";
-        portao2.temVidro = true;
-        System.out.println("Cor do Portão: " + portao1.cor + " | Tipo de material da estrutura " + portao1.materialEstrutura + "  | Possui Vidro: " + portao1.temVidro);
-        System.out.println("Cor do Portão: " + portao2.cor + " | Tipo de material da estrutura " + portao2.materialEstrutura + "  | Possui Vidro: " + portao2.temVidro);
+    	Portao portao1 = new Portao("Alumínio", "preto", false, false, true);
+    	Portao portao2 = new Portao("Aço", "cinza", true, true, false );
+        
+        
+       portao1.exibir();
+       portao2.exibir();
+        
+        portao1.abrir();
+        portao1.fechar();
+        portao1.trancar();
+       
+        portao2.destrancar();
+        portao2.abrir();
+        portao2.fechar();
+        portao2.trancar();
+        portao2.abrir();
+        
+        portao1.exibir();
+        portao2.exibir();
+    
     }
 }
--- br/com/fiapride/model/Portao.java
+++ br/com/fiapride/model/Portao.java
@@ -4,4 +4,55 @@ public class Portao {
 	public String materialEstrutura;
 	public String cor;
 	public boolean temVidro;
+	public boolean trancado;
+	public boolean aberto;
+	
+public Portao(String materialEstrutura, String cor, boolean temVidro, boolean trancado, boolean aberto) {
+    this.materialEstrutura = materialEstrutura;
+    this.cor = cor;
+    this.temVidro = temVidro;
+    this.trancado = trancado;
+    this.aberto = aberto;
+	}
+
+	public void abrir() {
+		if(trancado) {
+			System.out.println("Não foi possivel abrir o portão, destranque ele");
+		return;
+		}
+		if(trancado == false) {
+			System.out.println("Portão aberto com sucesso");
+			this.aberto = true;
+		}
+	}
+	
+	public void fechar() {
+		if(aberto == true) {
+			System.out.println("Fechando portão");
+			this.aberto = false;
+			return;
+		}
+	}
+	
+	public void trancar() {
+		if(this.aberto == false && this.trancado == false) {
+			System.out.println("Trancando o Portão");
+	}		
 }
+	public void destrancar() {
+		if(this.trancado == true) {
+			System.out.println("Destrancando o portão");
+			this.trancado = false;
+			return;
+		}
+	}
+	
+	public void exibir() {
+		System.out.print("Aberto: " + this.aberto );
+		System.out.print("Trancado: " + this.trancado);
+		System.out.print("Tem vidro: " + this.temVidro);
+		System.out.print("Material do Portão: " + this.materialEstrutura);
+		System.out.print("Cor: " + this.cor);
+	}
+}
+
