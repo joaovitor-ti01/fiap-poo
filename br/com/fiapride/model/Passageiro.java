d404d39bc20dbbd26c75623b0bae63d5429bcb5f
João Vitor Lima <joaolimacaldeira.ti@gmail.com>
2026-03-12 08:14:08 -0300

    fiap ride correct added

--- br/com/fiapride/model/Passageiro.java
+++ br/com/fiapride/model/Passageiro.java
@@ -1,13 +1,49 @@
 package br.com.fiapride.model;
 
-// A Classe define a estrutura. Ela não é o passageiro real, é apenas o modelo.
 public class Passageiro {
     
-    // Atributos (Características)
+    // Agora ninguém fora desta classe consegue mexer nisso diretamente!
+    private String nome;
+    private String cpf;
+    private double saldo;
     
-    public String nome;
-    public double saldo; 
-    
-    // Nota: Por enquanto usamos 'public' para facilitar o aprendizado.
-    // Nas próximas aulas, aprenderemos a proteger esses dados.
+    // ... métodos anteriores (adicionarSaldo) continuam aqui
+}
+
+public Passageiro(String nome, String cpf) {
+    this.setNome(nome);
+    this.setCpf(cpf);
+    this.setSaldo(0); // Todo mundo começa com zero!
+}
+
+// Método para PEGAR o saldo (Leitura)
+public double getSaldo() {
+    return this.saldo; // Apenas devolve o valor, não altera nada.
+}
+
+// Método para DEFINIR o saldo (Escrita com Regra de Negócio!)
+private void setSaldo(double valor) {
+    if (valor >= 0) {
+        this.saldo = valor;
+    } else {
+        System.out.println("Erro de Segurança: Tentativa de definir saldo negativo bloqueada!");
+    }
+}
+
+// Faça o mesmo para o nome (sem regras complexas por enquanto)
+public String getNome() {
+    return this.nome;
+}
+
+private void setNome(String nome) {
+    this.nome = nome;
+}
+
+// Faça o mesmo para o cpf (sem regras complexas por enquanto)
+public String getCpf() {
+    return this.cpf;
+}
+
+private void setCpf(String cpf) {
+    this.cpf = cpf;
 }
